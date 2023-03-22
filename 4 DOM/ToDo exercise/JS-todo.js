ab1e69122f60c226049dd7e05e1266a6650db533
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-03-22 16:05:23 +0900

    Complete assignment

--- 4 DOM/ToDo exercise/JS-todo.js
+++ 4 DOM/ToDo exercise/JS-todo.js
@@ -21,21 +21,49 @@ add new todo
  - add checkboxes to toggle strikeout
 */
 
-/*
-Mark todo as complete
-- auto attach button and checkbox to item
-- apply checkbox and button event handlers to UL
-- checkbox toggles strikethrough
-*/
+const taskForm = document.querySelector("#taskForm");
+const taskName = document.querySelector('input[name = "task"]');
+const taskList = document.querySelector("ul.taskList");
 
-/*
-Remove Item
-- remove button removes target list item
-*/
+var allListElements = localStorage.getItem("allLis")
 
-/*
-Save todos in localStorage
-- JSON.stringify() ul?
-- how to store HTML block in localStorage
-*/
+// If there exists a set of LI elements, set it as the contents of the UL
+if (allListElements){
+    taskList.innerHTML = allListElements;
+}
 
+// Add click event listeners to UL element for both buttons and list items
+taskList.addEventListener("click", function(event){
+    if (event.target.tagName === "BUTTON"){
+        event.target.parentElement.remove();
+    } else if (event.target.tagName === "LI") {
+        event.target.classList.toggle("complete");
+    }
+    localStorage.setItem("allLis", taskList.innerHTML);
+})
+
+// Create and return a list item with innerText set as argument passed in 
+// as "task", append remove button.
+function createToDo(task){
+    let newLi = document.createElement("li");
+    newLi.innerText = task;
+    newLi.classList.add("todoItem")
+    let newBtn = document.createElement("button");
+    newBtn.innerText = "X";
+    newLi.appendChild(newBtn)
+    return newLi;
+}
+
+// Add given list item to list passed in as argument
+function addToDo(listItem, list){
+    list.append(listItem);
+    localStorage.setItem("allLis", list.innerHTML);
+}
+
+// Add the user input to task list in the form of a todo line item
+taskForm.addEventListener("submit",  function(event){
+    event.preventDefault();
+    item = createToDo(taskName.value);
+    baseList = document.querySelector("ul")
+    addToDo(item, baseList);
+})
