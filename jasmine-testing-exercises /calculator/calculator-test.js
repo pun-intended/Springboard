67650b15cb29564eed9e64a4e03905c7ae3707f2
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-15 12:51:28 +0900

    Add functionality to test payment accuracy
    Test the functionality of calculateMonthlyPayment function

--- jasmine-testing-exercises /calculator/calculator-test.js
+++ jasmine-testing-exercises /calculator/calculator-test.js
@@ -1,11 +1,25 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let values1 = {
+    amount: 100000,
+    years: 10,
+    rate: .06,
+  }
+  let values2 = {
+    amount: 100,
+    years: 100,
+    rate: .06,
+  }
+  expect(calculateMonthlyPayment(values1)).toEqual("1110.21");
+  expect(calculateMonthlyPayment(values2)).toEqual(".50")
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+
 });
 
+afterEach(function(){
+  setupIntialValues();
+})
 /// etc
