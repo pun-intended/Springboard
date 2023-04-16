3ed3f42415d4906ea66caed6f4d6adf9c24b3c90
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-16 09:33:32 +0900

    Move test cases out of functions
    move example objects out of test cases to be used over all testing

--- jasmine-testing-exercises /calculator/calculator-test.js
+++ jasmine-testing-exercises /calculator/calculator-test.js
@@ -11,15 +11,42 @@ it('should calculate the monthly rate correctly', function () {
     rate: .06,
   }
   expect(calculateMonthlyPayment(values1)).toEqual("1110.21");
-  expect(calculateMonthlyPayment(values2)).toEqual(".50")
+  expect(calculateMonthlyPayment(values2)).toEqual("0.50")
 });
 
 
 it("should return a result with 2 decimal places", function() {
+  let zeroCents = {
+    amount: 100000,
+    years: 10,
+    rate: .06,
+  }
+  expect(calculateMonthlyPayment(zeroCents)).toEqual("1110.21");
 
+  expect(function(){
+  let monthlyPayment = document.getElementById("monthly-payment").innerText;
+  let decPlace = monthlyPayment.slice(-3).slice(0, 0);
+  console.log(decPlace)
+  return decPlace;
+  }).toEqual(".")
+  let testValues = {
+    amount: 100000,
+    years: 10,
+    rate: .06,
+  }
+  expect(calculateMonthlyPayment(testValues)).toEqual("1110.21");
 });
 
+it('should calculate for 0% interest'), function(){
+    let zeroInterest = {
+    amount: 100000,
+    years: 10,
+    rate: .0,
+  }
+  expect(calculateMonthlyPayment(zeroInterest)).toEqual("833.33");
+}
+
 afterEach(function(){
-  setupIntialValues();
+//  setupIntialValues();
 })
 /// etc
