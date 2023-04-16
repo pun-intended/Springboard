bf17cd21de5bdbea25a41ad084adb78984ad0346
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-16 16:58:57 +0900

    Add testing for updateServerTable
    Add testing for appending funtionality, and to ensure all inputs are appended to table.
    create function to populate server table for testing purposes

--- jasmine-testing-exercises /tip-pool/servers.test.js
+++ jasmine-testing-exercises /tip-pool/servers.test.js
@@ -1,4 +1,17 @@
 describe("Servers test (with setup and tear-down)", function() {
+
+});
+
+let serverList = ['wallace', 'gromit', 'seth', 'dean', 'kevin', 'scott'];
+
+function populateServerTable(){
+    serverList.forEach(element => {
+      serverNameInput.value = element;
+      submitServerInfo();
+    });
+}
+
+describe("SubmitServerInfo functionality tests", function(){
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -11,7 +24,33 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should reset input field on submitServerInfo()', function(){
+    submitServerInfo();
+    expect(serverNameInput.value).toEqual('');
+  })
+
+  it('should increment serverId after each submission', function(){
+    populateServerTable();
+    expect(serverId).toBe(serverList.length);
+  })
+
   afterEach(function() {
-    // teardown logic
+    allServers = {};
+    serverId = 0;
+
+    updateServerTable();
   });
-});
+})
+describe("updateServerTable test", function(){
+  it('should append server input to serverTable', function(){
+    let testName = 'Terry'
+    serverNameInput.value = testName
+    updateServerTable();
+    expect(serverTbody.rows.length).toEqual(1);
+    expect(serverTbody.rows[0].cells[0].innerText).toEqual(testName);
+  });
+  it('should append all inputs to serverTable', function(){
+    populateServerTable();
+    expect(serverTbody.rows.length).toEqual(serverList.length);
+  })
+})
