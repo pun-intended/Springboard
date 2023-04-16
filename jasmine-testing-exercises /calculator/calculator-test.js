91a79d05390a049d0f6f579b066fe80404c884ee
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-16 09:43:27 +0900

    Remove test objects from testing functions
    move testing object to the top level for use during testing.

--- jasmine-testing-exercises /calculator/calculator-test.js
+++ jasmine-testing-exercises /calculator/calculator-test.js
@@ -1,52 +1,39 @@
+let values1 = {
+  amount: 100000,
+  years: 10,
+  rate: .06,
+}
 
-it('should calculate the monthly rate correctly', function () {
-  let values1 = {
+let values2 = {
+  amount: 100,
+  years: 100,
+  rate: .06,
+}
+
+let zeroCents = {
+  amount: 100000,
+  years: 6,
+  rate: .010104,
+}
+
+let zeroInterest = {
     amount: 100000,
     years: 10,
-    rate: .06,
-  }
-  let values2 = {
-    amount: 100,
-    years: 100,
-    rate: .06,
-  }
+    rate: 0,
+}
+
+it('should calculate the monthly rate correctly', function () {
   expect(calculateMonthlyPayment(values1)).toEqual("1110.21");
   expect(calculateMonthlyPayment(values2)).toEqual("0.50")
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  let zeroCents = {
-    amount: 100000,
-    years: 10,
-    rate: .06,
-  }
-  expect(calculateMonthlyPayment(zeroCents)).toEqual("1110.21");
-
-  expect(function(){
-  let monthlyPayment = document.getElementById("monthly-payment").innerText;
-  let decPlace = monthlyPayment.slice(-3).slice(0, 0);
-  console.log(decPlace)
-  return decPlace;
-  }).toEqual(".")
-  let testValues = {
-    amount: 100000,
-    years: 10,
-    rate: .06,
-  }
-  expect(calculateMonthlyPayment(testValues)).toEqual("1110.21");
+  //ensure that zeros are still shown in decimla places
+  expect(calculateMonthlyPayment(zeroCents)).toEqual("1432.00");
 });
 
-it('should calculate for 0% interest'), function(){
-    let zeroInterest = {
-    amount: 100000,
-    years: 10,
-    rate: .0,
-  }
+it('should calculate for 0% interest', function(){
   expect(calculateMonthlyPayment(zeroInterest)).toEqual("833.33");
-}
-
-afterEach(function(){
-//  setupIntialValues();
-})
+});
 /// etc
