2ad8ffc5e1a5262f33e145dd77446a0bbd5d400d
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-20 21:43:33 +0900

    Detail tests to be created
    comment on test cases to be created and what should be tested.

--- jasmine-testing-exercises /tip-pool/helpers.test.js
+++ jasmine-testing-exercises /tip-pool/helpers.test.js
@@ -1,6 +1,19 @@
 /*
 Functions to test
 - sumPaymentTotal
+    - payment total correct
 - CalculateTipPercent
-- appendTd
+    - tip percent correct
 */
+describe('helper function tests', function(){
+
+    it('should return correct percentage calculation', function(){
+        expect(calculateTipPercent(100, 10)).toEqual(10);
+    })
+    it
+})
+
+afterEach( function(){
+    allPayments = {};
+    paymentId = 0;
+});
--- jasmine-testing-exercises /tip-pool/payments.test.js
+++ jasmine-testing-exercises /tip-pool/payments.test.js
@@ -1,7 +1,25 @@
 /*
 Functions to test
 - submitPaymentInfo
+    - payment added to table
+    - changes reflected in server table
+    - changes reflected in summary table
 - createCurPayment
+    - return valid payment object
 - appendPaymentTable
+    - payment is added to end of table
 - updateSummary
+    - change in payment data reflected in summary
 */
+
+describe('submit payment info testing', function(){
+    it("should create a valid payment object with bill, tip and tip percent attributes", 
+    function(){
+        let testPayment = {
+            billAmt : 500,
+            tipAmt : 50,
+            tipPercent : calculateTipPercent(billAmt, tipAmt)
+        }
+
+    })
+})
