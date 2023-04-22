1772c0ef09a1c7a4b5c9a51b45660131c566da6f
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-22 19:59:42 +0900

    Add testing for all functions
    add tests for appendPaymentTable, submitPaymentInfo, createCurPayment, updateSummary

--- jasmine-testing-exercises /tip-pool/payments.test.js
+++ jasmine-testing-exercises /tip-pool/payments.test.js
@@ -1,25 +1,71 @@
 /*
 Functions to test
-- submitPaymentInfo
-    - payment added to table
-    - changes reflected in server table
-    - changes reflected in summary table
-- createCurPayment
-    - return valid payment object
-- appendPaymentTable
-    - payment is added to end of table
 - updateSummary
     - change in payment data reflected in summary
 */
+billTestArray = [100, 700, 500];
+tipTestArray = [10, 150, 500];
 
-describe('submit payment info testing', function(){
-    it("should create a valid payment object with bill, tip and tip percent attributes", 
-    function(){
-        let testPayment = {
-            billAmt : 500,
-            tipAmt : 50,
-            tipPercent : calculateTipPercent(billAmt, tipAmt)
-        }
-
+describe('createCurPayment testing', function(){
+    it('should create a payment object with the correct values', function(){
+        billAmtInput.value = billTestArray[0];
+        tipAmtInput.value = tipTestArray[0];
+        let testPayment = createCurPayment();
+        expect(testPayment['billAmt']).toEqual('100');
+        expect(testPayment['tipAmt']).toEqual('10');
+        expect(testPayment['tipPercent']).toEqual(10);
     })
 })
+
+describe('submitPaymentInfo testing', function(){
+    it('should create a payment object', function(){
+        billAmtInput.value = billTestArray[0];
+        tipAmtInput.value = tipTestArray[0];
+        submitPaymentInfo();
+        expect(Object.keys(allPayments)).toContain('payment1');
+    })
+    it('should append changes to payment tables', function(){
+        billAmtInput.value = billTestArray[0];
+        tipAmtInput.value = tipTestArray[0];
+        submitPaymentInfo();        
+        expect(document.getElementById('payment1')).toBeTruthy();
+    })
+})
+
+describe('appendPaymentTable testing', function(){
+    it('should append the payment information to the end of paymentTBody', function(){
+        for (let i = 0; i < billTestArray.length; i++){
+            billAmtInput.value = billTestArray[i];
+            tipAmtInput.value = tipTestArray[i];
+            submitPaymentInfo();  
+        }      
+        expect(paymentTbody.lastChild.id).toEqual('payment' + billTestArray.length);
+    })
+})
+
+describe('updateSummary testing', function(){
+    it('should display the correct totals in the summary section', function(){
+        for (let i = 0; i < billTestArray.length; i++){
+            billAmtInput.value = billTestArray[i];
+            tipAmtInput.value = tipTestArray[i];
+            submitPaymentInfo();  
+        } 
+        billTotal = billTestArray.reduce((a, b) => a + b, 0);
+        tipTotal = tipTestArray.reduce((a, b) => a + b, 0);
+        tipAvg = Math.round(sumPaymentTotal('tipPercent') / Object.keys(allPayments).length);
+        console.log(tipAvg);
+        
+
+        expect(summaryTds[0].innerHTML).toEqual('$' + billTotal);
+        expect(summaryTds[1].innerHTML).toEqual('$' + tipTotal);
+        expect(summaryTds[2].innerHTML).toEqual(tipAvg + '%');
+    })
+    
+})
+
+afterEach(function() {
+    allServers = {};
+    serverId = 0;
+    updateServerTable();
+    updateSummary();
+  });
