b8706854ecad8ff7827018836300e5deee836fe6
pun-intended <31040416+pun-intended@users.noreply.github.com>
2023-04-22 20:20:05 +0900

    Add tests for functions
    Add tests for CalculateTipPercent, sumPaymentTotal

--- jasmine-testing-exercises /tip-pool/helpers.test.js
+++ jasmine-testing-exercises /tip-pool/helpers.test.js
@@ -4,13 +4,41 @@ Functions to test
     - payment total correct
 - CalculateTipPercent
     - tip percent correct
+- appendTd
 */
-describe('helper function tests', function(){
+
+
+billTestArray = [100, 700, 500, 90];
+tipTestArray = [10, 150, 500, 30];
+
+describe('CalculateTipPercent testing', function(){
 
     it('should return correct percentage calculation', function(){
         expect(calculateTipPercent(100, 10)).toEqual(10);
+        expect(calculateTipPercent(101, 3)).toEqual(3);
     })
-    it
+})
+
+describe('sumPaymentTotal testing', function(){
+    it('should return the correct totals for all components', function(){
+        for (let i = 0; i < billTestArray.length; i++){
+            billAmtInput.value = billTestArray[i];
+            tipAmtInput.value = tipTestArray[i];
+            submitPaymentInfo();  
+        } 
+
+        billTotal = billTestArray.reduce((a, b) => a+b, 0);
+        tipTotal = tipTestArray.reduce((a, b) => a+b, 0);
+        tipPercentTotal = 0;
+        for (let i = 0; i < billTestArray.length; i++){
+            tipPercentTotal += Math.round(100/(billTestArray[i]/tipTestArray[i]))
+        }
+
+        expect(sumPaymentTotal('billAmt')).toEqual(billTotal);
+        expect(sumPaymentTotal('tipAmt')).toEqual(tipTotal);
+        expect(sumPaymentTotal('tipPercent')).toEqual(tipPercentTotal);
+    })
+
 })
 
 afterEach( function(){
